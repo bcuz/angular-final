adbbb3f57bc341efd56c5f3114c03791008c8375
Adam Nawrocki <adam.nawroc@gmail.com>
2015-11-19 07:45:52 -0500

    Use angular way to get title from input field

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -4,11 +4,11 @@ app.controller('HomeController', ['$scope', 'suggestions', function($scope, sugg
 
     $scope.addSuggestion = function(index) {
     $scope.posts.push({
-    title: '',
+    title: $scope.title,
     upvotes: 0,
     comments: [],
     })
-      $scope.posts[$scope.posts.length -1].title = $(".form-control").val();
+
 };
     }]);
 
