85cc356ee999411bca562c00fe5ec479448e91c8
Adam Nawrocki <adam.nawroc@gmail.com>
2015-11-19 07:30:30 -0500

    Start addSuggestion function

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,4 +1,13 @@
 app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
     $scope.posts = suggestions.posts;
+
+
+    $scope.addSuggestion = function(index) {
+      $scope.posts.push({
+    title: 'Retrofit water fountain with Gatorade',
+    upvotes: 7,
+    comments: [],
+    })
+};
     }]);
 
