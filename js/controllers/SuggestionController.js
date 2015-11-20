a89b001044fe9977c22529a91d0f4e7dbc6cbdfd
Adam Nawrocki <adam.nawroc@gmail.com>
2015-11-20 09:42:32 -0500

    Finished attempt at SuggestionController

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,4 +1,14 @@
-app.controller('SuggestionController', ['$scope', 'suggestions', function($scope, suggestions) {
+app.controller('SuggestionController', ['$scope', 'suggestions', '$routeParams', function($scope, suggestions, $routeParams) {
+    $scope.post = suggestions.posts[$routeParams.id];
 
+    $scope.addComment = function(comment) {
+    post.comments.push({
+        body: comment,
+        upvotes: 0
+    })
+};
+    $scope.upVote = function(comment) {
+    comment.upvotes += 1;
+};
     }]);
 
