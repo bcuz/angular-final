5fa4045177c9440a556fb64a71b9265cc25610aa
Adam Nawrocki <adam.nawroc@gmail.com>
2015-11-20 08:13:33 -0500

    Complete upvoting function

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -17,8 +17,8 @@ app.controller('HomeController', ['$scope', 'suggestions', function($scope, sugg
 
 };
 
-    $scope.upVote = function(index) {
-    $scope.posts[index].upvotes += 1;
+    $scope.upVote = function(post) {
+    post.upvotes += 1;
 };
     }]);
 
