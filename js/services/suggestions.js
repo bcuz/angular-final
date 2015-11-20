03e93a2b2ee232fd2b9c25c4fa226de799f814f6
Adam Nawrocki <adam.nawroc@gmail.com>
2015-11-20 07:32:55 -0500

    Put posts in original order

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -14,15 +14,15 @@
     comments: [],
     },
     {
-    title: 'Sing Bon Jovi\'s "Living on a Prayer" halfway through meetings',
-    upvotes: 3,
-    comments: [],
-    },
-    {
     title: 'Retrofit water fountain with Gatorade',
     upvotes: 7,
     comments: [],
     },
+    {
+    title: 'Sing Bon Jovi\'s "Living on a Prayer" halfway through meetings',
+    upvotes: 3,
+    comments: [],
+    }
     ]
     };
     return demoSuggestions;
